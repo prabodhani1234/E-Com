4175c2ca7c663a2194ebb0bcecba0616cb2496c4
prabodhani1234 <83935882+prabodhani1234@users.noreply.github.com>
2023-09-03 09:23:21 +0530

    initial commit

---
+++ Backend/abd.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const abd = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default abd
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+import './App.css';
+import Header from './component/Header';
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <main className='pt-16'>
+        <Outlet/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
